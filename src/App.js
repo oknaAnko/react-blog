981e3271d3872257b0d83539cc878fe17fd8276a
Anko <anna.jedrzejko@op.pl>
2021-07-17 21:38:13 +0200

    feat: Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { fetchPosts } from './redux/actions';
+
+import HomePage from './pages/HomePage';
+import PostPage from './pages/PostPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 import './App.scss';
 
@@ -13,7 +18,15 @@ const App = () => {
     }, [])
 
     return (
-        <div>This is a blog</div>
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/posty/:id" component={PostPage} />
+                    <Route path="" component={NotFoundPage} />
+                </Switch>
+            </div>
+        </Router>
     );
 }
 export default App;
---
+++ src/pages/PostPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PostPage = () => {
+    return (
+        <div>this is PostPage</div>
+    );
+}
+
+export default PostPage;
