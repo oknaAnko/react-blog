29065928fe396e98b39e425d610ed1820677329e
Anko <anna.jedrzejko@op.pl>
2021-07-17 12:25:13 +0200

    feat: Create store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
-    ReactDOM.render(<App />, rootElement)
+    ReactDOM.render(
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>,
+        rootElement)
 }
---
+++ src/redux/postReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    isLoading: false,
+    posts: [],
+    error: null
+};
+
+export const postReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+};
