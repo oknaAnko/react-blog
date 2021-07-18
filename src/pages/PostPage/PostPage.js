b7e619e854be3b66b851e3af1685b16c88008a11
Anko <anna.jedrzejko@op.pl>
2021-07-18 21:15:38 +0200

    feat: Handle errors and loading in Post Page

--- src/pages/PostPage/PostPage.js
+++ src/pages/PostPage/PostPage.js
@@ -14,6 +14,9 @@ const style = bemCssModules(PostPageStyles);
 const PostPage = ({ match }) => {
     const allPosts = useSelector(state => state.posts.posts);
     const allComments = useSelector(state => state.comments.commentsList.comments);
+    const isLoading = useSelector(state => state.comments.commentsList.isLoading);
+    const isCommentListError = useSelector(state => state.comments.commentsList.error);
+    const isNewCommentError = useSelector(state => state.comments.newComment.error);
 
     const postDetails = allPosts
         .filter(post => post.id === Number(match.params.id))
@@ -38,9 +41,13 @@ const PostPage = ({ match }) => {
             {postDetails}
             <section>
                 <h4 className={style('title')}>Komentarze</h4>
+                {isLoading && <p className={style('text')}>Trwa ładowanie komentarzy...</p>}
                 <ul>
                     {postComments}
                 </ul>
+                {isCommentListError && <p className={style('text')}>Przepraszamy, nie można wyświetlić komentarzy</p>}
+                {isNewCommentError && <p className={style('text')}>Przepraszamy, nie można dodać komentarza</p>}
+
                 <button className={style('btn')} onClick={handleToggleVisibleClick}>{setBtnLabel}</button>
                 {isVisible && <CommentForm postId={match.params.id}
                     currentComments={currentComments}
