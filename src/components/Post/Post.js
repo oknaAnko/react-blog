4cd6b911e94660e971130d64d0eb23138d5d467d
Anko <anna.jedrzejko@op.pl>
2021-12-06 20:11:12 +0100

    refactor: Memoize Post component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import bemCssModules from 'bem-css-modules';
-import { default as PostStyles } from './Post.module.scss';
+import React from "react";
+import bemCssModules from "bem-css-modules";
+import { default as PostStyles } from "./Post.module.scss";
 
 const style = bemCssModules(PostStyles);
 
 const Post = ({ userId, title, body }) => {
-    return (
-        <section >
-            <h3 className={style('title')}>{title}</h3>
-            <p className={style('post-body')}>{body}</p>
-            <p className={style('text')}>Post dodany przez: User {userId}</p>
-        </section>
-    );
-}
+  console.log("post render");
+  return (
+    <section>
+      <h3 className={style("title")}>{title}</h3>
+      <p className={style("post-body")}>{body}</p>
+      <p className={style("text")}>Post dodany przez: User {userId}</p>
+    </section>
+  );
+};
 
-export default Post;
+export default React.memo(Post);
