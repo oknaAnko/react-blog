a9a4b23a0690f766a45f76cee40842d9872b1849
Anko <anna.jedrzejko@op.pl>
2022-03-30 21:54:19 +0200

    styles: Home page

--- src/components/PostTitleLink/PostTitleLink.js
+++ src/components/PostTitleLink/PostTitleLink.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import bemCssModules from 'bem-css-modules';
-import { default as PostTitleLinkStyles } from './PostTitleLink.module.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import bemCssModules from "bem-css-modules";
+import { default as PostTitleLinkStyles } from "./PostTitleLink.module.scss";
 
 const style = bemCssModules(PostTitleLinkStyles);
 
 const PostTitleLink = ({ userId, id, title }) => {
-
-    return (
-        <li className={style()}>
-            <Link to={`/posty/${id}`} className={style('link')}>{title}</Link>
-            <p className={style('text')}>Dodany przez: User {userId}</p>
-        </li>
-    );
-}
+  return (
+    <li className={style()}>
+      <div className={style("bgc")}>
+        <Link to={`/posty/${id}`} className={style("link")}>
+          {title}
+        </Link>
+        <p className={style("text")}>Dodany przez: User {userId}</p>
+      </div>
+    </li>
+  );
+};
 
 export default PostTitleLink;
